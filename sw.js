d95cf88b1b5d0f87e4dbad5c8a00942411199eef
StarPool <seitetsu@bembegi.com>
2021-04-26 15:04:29 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
 
 workbox.routing.registerRoute(
-    '/',
+    '/qrcode',
     new workbox.strategies.NetworkFirst()
 )
 
